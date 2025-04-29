fd6aa04e648f82cd95933421ec6b0112cce39df5
tommclark <tom.m.clark2003@gmail.com>
2025-04-29 14:30:52 +0100

    server modifications

--- server/server.js
+++ server/server.js
@@ -5,16 +5,6 @@ const port = 3000;
 
 app.use(express.static('public'));
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
-
-});
-
-app.get('/calibration.html', (req, res) => {
-    res.sendFile(path.join(__dirname, 'calibration.html'));
-
-});
-
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}/`);
 }); 
